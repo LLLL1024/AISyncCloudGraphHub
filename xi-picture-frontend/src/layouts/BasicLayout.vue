0ac93beabce795355f05555482e339d6ebc46f34
LLLL1024 <1611857077@qq.com>
2024-12-22 11:11:57 +0800

    项目初始化-前端

---
+++ xi-picture-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,56 @@
+<!-- 全局通用布局：基础布局结构 -->
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <!-- 全局顶部栏-->
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <!-- 动态路由替换-->
+        <router-view />
+      </a-layout-content>
+      <!-- 全局底部栏-->
+      <a-layout-footer class="footer">
+        <a href="https://github.com/LLLL1024/AISyncCloudGraphHub" target="_blank">
+          云图库 by 程序员兮颜
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+/**
+#:id选择器，.:类选择器，另外一个是元素选择器
+#basicLayout .footer:这个选择器表示选择ID为basicLayout的元素内的所有类名为footer的元素。这是一种后代选择器，意味着.footer元素必须是#basicLayout元素的子元素、孙元素等，但不一定是直接子元素。
+**/
+
+#basicLayout .header {
+  padding-inline: 20px;
+  margin-bottom: 16px;
+  color: unset;
+  background: white;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  padding: 20px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+
---
+++ xi-picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = '欢迎来到云图汇，你将从这里获取免费图片~'
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
