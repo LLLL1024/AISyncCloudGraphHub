d5d2d3dc756424f46e3dc97efc0fbf0143d95ade
LLLL1024 <1611857077@qq.com>
2024-12-22 19:27:51 +0800

    图片模块-前端

---
+++ xi-picture-frontend/src/pages/AddPicturePage.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="addPicturePage">
+    <h2 style="margin-bottom: 16px">
+      {{ route.query?.id ? '修改图片' : '创建图片' }}
+    </h2>
+    <!-- 图片上传组件 -->
+    <PictureUpload :picture="picture" :onSuccess="onSuccess" />
+    <!-- 图片信息表单 每个form-item都要有name -->
+    <a-form
+      v-if="picture"
+      name="pictureForm"
+      layout="vertical"
+      :model="pictureForm"
+      @finish="handleSubmit"
+    >
+      <a-form-item name="name" label="名称">
+        <a-input v-model:value="pictureForm.name" placeholder="请输入名称" allow-clear />
+      </a-form-item>
+      <a-form-item name="introduction" label="简介">
+        <a-textarea
+          v-model:value="pictureForm.introduction"
+          placeholder="请输入简介"
+          :auto-size="{ minRows: 2, maxRows: 5 }"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="category" label="分类">
+        <a-auto-complete
+          v-model:value="pictureForm.category"
+          placeholder="请输入分类"
+          :options="categoryOptions"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item name="tags" label="标签">
+        <a-select
+          v-model:value="pictureForm.tags"
+          mode="tags"
+          placeholder="请输入标签"
+          :options="tagOptions"
+          allow-clear
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 100%">创建</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PictureUpload from '@/components/PictureUpload.vue'
+import { onMounted, reactive, ref } from 'vue'
+import { message } from 'ant-design-vue'
+import {
+  editPictureUsingPost,
+  getPictureVoByIdUsingGet,
+  listPictureTagCategoryUsingGet,
+} from '@/api/pictureController.ts'
+import { useRoute, useRouter } from 'vue-router'
+
+const picture = ref<API.PictureVO>()
+const pictureForm = reactive<API.PictureEditRequest>({})
+
+/**
+ * 图片上传成功
+ * @param newPicture
+ */
+const onSuccess = (newPicture: API.PictureVO) => {
+  picture.value = newPicture
+  pictureForm.name = newPicture.name
+}
+
+const router = useRouter() // 实现页面（路由）的跳转
+
+/**
+ * 提交表单
+ * @param values
+ */
+const handleSubmit = async (values: any) => {
+  console.log(values)
+  const pictureId = picture.value.id
+  if (!pictureId) {
+    return
+  }
+  const res = await editPictureUsingPost({
+    id: pictureId,
+    ...values,
+  })
+  // 操作成功
+  if (res.data.code === 0 && res.data.data) {
+    message.success('创建成功')
+    // 跳转到图片详情页
+    router.push({
+      path: `/picture/${pictureId}`,
+    })
+  } else {
+    message.error('创建失败，' + res.data.message)
+  }
+}
+
+const categoryOptions = ref<string[]>([])
+const tagOptions = ref<string[]>([])
+
+/**
+ * 获取标签和分类选项
+ * @param values
+ */
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    tagOptions.value = (res.data.data.tagList ?? []).map((data: string) => {
+      return {
+        value: data,
+        label: data,
+      }
+    })
+    categoryOptions.value = (res.data.data.categoryList ?? []).map((data: string) => {
+      return {
+        value: data,
+        label: data,
+      }
+    })
+  } else {
+    message.error('获取标签分类列表失败，' + res.data.message)
+  }
+}
+
+// 首次加载页面的时候执行一次
+onMounted(() => {
+  getTagCategoryOptions()
+})
+
+const route = useRoute() // 与useRouter不一样，获取信息（用于访问当前的路由信息）
+
+// 获取老数据
+const getOldPicture = async () => {
+  // 获取到 id，如 http://localhost:5173/add_picture?id=1870759205142036481，id：1870759205142036481
+  // 它将当前的路由导航到 /add_picture 路径，并且通过 URL 参数传递了一个 id。
+  const id = route.query?.id
+  if (id) {
+    const res = await getPictureVoByIdUsingGet({
+      id,
+    })
+    if (res.data.code === 0 && res.data.data) {
+      const data = res.data.data
+      picture.value = data
+      pictureForm.name = data.name
+      pictureForm.introduction = data.introduction
+      pictureForm.category = data.category
+      pictureForm.tags = data.tags
+    }
+  }
+}
+
+// 首次加载页面的时候执行一次
+onMounted(() => {
+  getOldPicture()
+})
+</script>
+
+<style scoped>
+#addPicturePage {
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
